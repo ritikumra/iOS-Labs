e2a3fd7123d5b5cbbf5a822a74ff15c069114d70
Riti Kumra <102514662+ritikumra@users.noreply.github.com>
2024-04-05 20:12:56 +0530

    Add files via upload

---
+++ 1 - Protocols/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+/*:
+## Exercise - Adopt Protocols: CustomStringConvertible, Equatable, and Comparable
+ 
+ Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
+ */
+import Foundation
+class Human: CustomStringConvertible, Equatable, Comparable, Codable{
+    var name :String
+    var age : Int
+    var description: String{
+        return "Hi! I am \(name) and I am \(age) years old"
+    }
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    static func < (lhs:Human,rhs:Human)->Bool{
+        return lhs.age < rhs.age
+    }
+    static func == (lhs:Human,rhs:Human)->Bool{
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+}
+var human1 = Human(name: "Riti", age: 21)
+var human2 = Human(name: "Ashu", age: 21)
+//:  Make the `Human` class adopt the `CustomStringConvertible` protocol. Print both of your previously initialized `Human` objects.
+
+print(human1)
+print(human2)
+//:  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
+
+print(human1 == human2)
+print(human1 != human2)
+//:  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
+var human3 = Human(name: "Kunal", age: 20)
+let people = [human1,human2,human3]
+let sortedPeople = people.sorted(by: <)
+for people in sortedPeople{
+    print(people)
+}
+
+//:  Make the `Human` class adopt the `Codable` protocol. Create a `JSONEncoder` and use it to encode as data one of the `Human` objects you have initialized. Then use that `Data` object to initialize a `String` representing the data that is stored, and print it to the console.
+let jsonEncoder = JSONEncoder()
+if let jsonData = try? jsonEncoder.encode(human1),
+ let jsonString = String(data: jsonData, encoding:.utf8){
+     print(jsonString)
+}
+
+/*:
+page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
+ */
---
+++ 1 - Protocols/Lab - Protocols.playground/Pages/4. App Exercise - Similar Workouts.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/*:
+## App Exercise - Similar Workouts
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ Previously in app exercises, you've used inheritance to enforce similar behavior between different kinds of workouts. However, you can enforce this behavior between different workouts using protocols.
+ 
+ Create a protocol `Workout` with two requirements: a settable property called `distance` and a settable property called `time`, both of type `Double`.
+ */
+
+protocol Workout {
+    var distance : Double {get}
+    var time : Double {get}
+}
+
+//:  Create two structs, `RunningWorkout` and `SwimmingWorkout`. Both should conform to the `Workout` protocol. Uncomment the function `simpleWorkoutDescription` below, create an instance of each of your structs, and call `simpleWorkoutDescription` twice, passing in a `RunningWorkout` object and then a `SwimmingWorkout` object.
+func simpleWorkoutDescription(workout: Workout) {
+    print("You went \(workout.distance) meters in \(workout.time) seconds.")
+}
+struct RunningWorkout: Workout{
+    var distance: Double {
+        return 0.0
+    }
+    var time: Double {
+        return 0.0
+    }
+    
+}
+struct SwimmingWorkout: Workout{
+    var distance: Double {
+        return 2.0
+    }
+    var time: Double {
+        return 2.0
+    }
+    
+}
+var runningWorkout = RunningWorkout()
+var swimmingWorkout = SwimmingWorkout()
+simpleWorkoutDescription(workout: runningWorkout)
+simpleWorkoutDescription(workout: swimmingWorkout)
+
+/*:
+[Previous](@previous)  |  page 4 of 5  |  [Next: App Exercise - Heart Rate Delegate](@next)
+ */
